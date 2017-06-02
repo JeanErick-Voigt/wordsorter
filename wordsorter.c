216b8292ce482e9931c4885dd2615c681e9ba4cf
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-06-02 10:02:23 -0400

    first commit

---
+++ wordsorter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char ** argv)
+{
+	char line[100]; 
+	char *p;
+	s = argv[i]
+	FILE *fp = (argv[i], "r");
+	for(int i = 1; i < argc, i++){
+		while(fgets(line, 100, fp) != NULL){
+			p = strtok(" ");
+			while(p != NULL){
+				printf("%s\n", p);
+			}
+		}
+	}
+}
